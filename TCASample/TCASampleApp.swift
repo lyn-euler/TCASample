376937ed3edaa06f5958c601cf1e476a9f2f575a
ly0u <asynclog@163.com>
2022-05-18 10:29:39 +0800

    Initial Commit

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by ello on 2022/5/18.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
